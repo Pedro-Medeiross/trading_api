ed3cb08d5a6ab3c472d0805ac152fa8f99d890c1
Pedro Medeiros <pedro.contato166@hotmail.com>
2025-06-21 22:48:40 -0300

    adjusting all

--- models/bot_options.py
+++ models/bot_options.py
@@ -6,7 +6,7 @@ class BotOptions(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, nullable=False)
-    bot_status = Column(Boolean, default=False)
+    bot_status = Column(Integer, default=False)
     stop_loss = Column(Integer, default=0)
     stop_win = Column(Integer, default=0)
     entry_price = Column(Integer, default=0)
